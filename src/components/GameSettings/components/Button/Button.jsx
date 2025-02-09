279df1c695c8df6c7772ddbcba444bb99c00a67c
ZICHENGYE48 <38489984+ZICHENGYE48@users.noreply.github.com>
2025-02-09 01:48:25 +0000

    Deploying to gh-pages from @ ZICHENGYE48/Toy-Robot-Simulator@1294ea4ff6a5ded5d413a9959c345b76394e5e3d 🚀

--- src/components/GameSettings/components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-const Button = ({
-  children,
-  onClick
-}) => (
-  <button className="bg-blue-600 w-full text-white px-6 py-2 rounded-full hover:bg-blue-700 transition duration-200" role="button" onClick={onClick}>{children}</button>
-)
-
-export default Button
